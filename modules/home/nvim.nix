929df165683012bef7a4257f0351f20427ba14c8
XJIE6 <fazz25@duck.com>
2024-11-13 15:12:43 +0300

    add nvim-surround

--- modules/home/nvim.nix
+++ modules/home/nvim.nix
@@ -33,6 +33,17 @@
     extraPlugins =  '' 
     return {
       { 
+        {
+        "kylechui/nvim-surround",
+          version = "*", -- Use for stability; omit to use `main` branch for the latest features
+            event = "VeryLazy",
+          config = function()
+            require("nvim-surround").setup({
+                -- Configuration here, or leave empty to use defaults
+                })
+        end 
+        },
+
         "ngtuonghy/runner-nvchad",
         config = function()
           require("runner-nvchad").setup{
